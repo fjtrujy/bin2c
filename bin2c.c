d5ac99cd37cc21d39c80a9360b0fa7d6f5f3e49a
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2024-03-27 08:45:48 +0100

    tmp

--- bin2c.c
+++ bin2c.c
@@ -1,72 +1,32 @@
 /*
-# _____     ___ ____     ___ ____
-#  ____|   |    ____|   |        | |____|
-# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
-#-----------------------------------------------------------------------
-# Copyright 2001-2024, ps2dev - http://www.ps2dev.org
-# Licenced under Academic Free License version 2.0
-# Review ps2sdk README & LICENSE files for further details.
+    main.c	- Main program file.
 */
 
+#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    size_t fd_size, i;
-	FILE *source,*dest;
-    unsigned char *buffer;
+    int result;
+    FILE *OutputFile;
+    unsigned int i, FilesAffected;
+    char filename[11];
+    struct FileEntry *file;
+	char *folder = "roms";
 
-	if(argc != 4) {
-		printf("bin2c - from bin2s By fjtrujy\n"
-			   "Usage: bin2c infile outfile label\n\n");
-		return 1;
-	}
+    printf("PlayStation 2 ROM image generator v1.12\n"
+           "Compiled on " __DATE__ " - " __TIME__ "\n"
+           "---------------------------------------\n\n");
 
-	if((source=fopen( argv[1], "rb")) == NULL) {
-		printf("Error opening %s for reading.\n",argv[1]);
-		return 1;
-	}
+#ifdef __MINGW32__
+                mkdir(folder);
+#else
+                mkdir(folder, 0755);
+#endif
+                chdir(folder);
 
-	fseek(source,0,SEEK_END);
-	fd_size = ftell(source);
-	fseek(source,0,SEEK_SET);
-
-	buffer = malloc(fd_size);
-	if(buffer == NULL) {
-		printf("Failed to allocate memory.\n");
-		fclose(source);
-		return 1;
-	}
-
-	if(fread(buffer,1,fd_size,source) != fd_size) {
-		printf("Failed to read file.\n");
-		fclose(source);
-        free(buffer);
-		return 1;
-	}
-	fclose(source);
-
-	if((dest = fopen(argv[2],"w+")) == NULL) {
-		printf("Failed to open/create %s.\n",argv[2]);
-        free(buffer);
-		return 1;
-	}
-
-	fprintf(dest, "#ifndef __%s__\n", argv[3]);
-	fprintf(dest, "#define __%s__\n\n", argv[3]);
-	fprintf(dest, "unsigned int size_%s = %zu;\n", argv[3], fd_size);
-	fprintf(dest, "unsigned char %s[] __attribute__((aligned(16))) = {", argv[3]);
-
-	for(i=0;i<fd_size;i+=1) {
-		if((i % 16) == 0) fprintf(dest, "\n\t");
-		fprintf(dest, "0x%02x, ", buffer[i]);
-	}
-
-	fprintf(dest, "\n};\n\n#endif\n");
-
-    free(buffer);
-	fclose(dest);
-
-	return 0;
+    return result;
 }
